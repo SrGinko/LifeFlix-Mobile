568db3ef62f05a559c0dea2df1bfa17e15bd4aec
SrGinko <pedeohenriquecardoso@gmail.com>
2025-10-11 07:57:51 -0300

    inicializando repositório

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,11 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-    return (
-        <Stack screenOptions={{
-            headerShown: false,
-        }}>
-        </Stack>
-    );
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="home/index" />    {/* Página inicial */}
+      <Stack.Screen name="home/filmes" />   {/* Filmes */}
+      <Stack.Screen name="canais/canais" /> {/* Canais */}
+    </Stack>
+  );
 }
