38a598b67a4615a1068880859f6d32e570f63ddf
SrGinko <pedeohenriquecardoso@gmail.com>
2026-01-31 04:51:22 -0300

    alterações no código layoute do app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,10 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="home/index" />    {/* Página inicial */}
-      <Stack.Screen name="home/filmes" />   {/* Filmes */}
-      <Stack.Screen name="canais/canais" /> {/* Canais */}
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="canal" />
+      <Stack.Screen name="perfil" />
+      <Stack.Screen name="settings" />
     </Stack>
   );
 }
