2e821e5a38b9a1a1a52f2bc4ef9deccc205c6b16
SrGinko <pedeohenriquecardoso@gmail.com>
2026-01-31 04:52:42 -0300

    página de favoritos

---
+++ app/favoritos.tsx
@@ -0,0 +1,25 @@
+import ButtonLink from "@/components/buttonLink";
+import CardFav from "@/components/favoritoCardComponent";
+import { styles } from "@/constants/styles";
+import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
+import { ScrollView, View, Text, TouchableOpacity } from "react-native";
+
+export default function Favoritos() {
+    return (
+        <View style={styles.container}>
+            <View style={[styles.header, { gap: 5 }]}>
+                <TouchableOpacity
+                    onPress={() => router.replace('/(tabs)/home')}
+                    style={{ padding: 5, marginLeft: 5 }}
+                >
+                    <Ionicons name="arrow-back" size={25} color={"#fff"} />
+                </TouchableOpacity>
+                <Text style={styles.titulo}>Favoritos</Text>
+            </View>
+            <ScrollView style={{ margin: 5 }}>
+                <CardFav />
+            </ScrollView>
+        </View>
+    )
+}
