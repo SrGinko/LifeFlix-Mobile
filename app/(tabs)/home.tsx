c5fc10e4235b2355b75f23be2cb2c93de2ebb5ca
SrGinko <pedeohenriquecardoso@gmail.com>
2026-03-30 05:34:36 -0300

    corrige os caminhos de importação dos componentes no arquivo home.tsx

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,12 +1,12 @@
-import Card from '@/components/CardComponent'
-import CardFav from '@/components/favoritoCardComponent'
-import { MenuLateral } from '@/components/menuLateralComponent'
-import SeparatorComponent from '@/components/separatorComponent'
+import Card from '@/components/layout/CardComponent'
+import CardFav from '@/components/layout/favoritoCardComponent'
+import { MenuLateral } from '@/components/layout/menuLateralComponent'
+import SeparatorComponent from '@/components/layout/separatorComponent'
 import { styles } from '@/constants/styles'
 import { Ionicons } from '@expo/vector-icons'
 import { router } from 'expo-router'
 import { useState } from 'react'
-import { View, Text, ScrollView, TouchableOpacity, Pressable } from 'react-native'
+import { View, Text, ScrollView, Pressable } from 'react-native'
 
 export default function Home() {
       const [menuOpen, setMenuOpen] = useState(false);
@@ -16,7 +16,7 @@ export default function Home() {
                 <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#fff', margin: 5, marginLeft: 10 }} >LifeFlix</Text>
                 <Pressable 
                 onPress={() => setMenuOpen(true)}
-                style={{backgroundColor: '#141414', justifyContent: 'center', alignItems: 'center', padding: 10, borderRadius:10, margin: 5}}>
+                style={{justifyContent: 'center', alignItems: 'center', padding: 10, borderRadius:10, margin: 5}}>
                     <Ionicons name='menu' size={25} color={"#fff"} />
                 </Pressable>
             </View>
