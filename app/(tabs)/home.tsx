25190cccb1646e3d3f717462924e1293c55f380b
SrGinko <pedeohenriquecardoso@gmail.com>
2026-04-23 06:11:15 -0300

    refatora o componente Home para melhorar a legibilidade e a estrutura do código

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -9,28 +9,36 @@ import { useState } from 'react'
 import { View, Text, ScrollView, Pressable } from 'react-native'
 
 export default function Home() {
-      const [menuOpen, setMenuOpen] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
     return (
         <View style={styles.container}>
             <View style={[styles.header, { justifyContent: 'space-between' }]}>
                 <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#fff', margin: 5, marginLeft: 10 }} >LifeFlix</Text>
-                <Pressable 
-                onPress={() => setMenuOpen(true)}
-                style={{justifyContent: 'center', alignItems: 'center', padding: 10, borderRadius:10, margin: 5}}>
+                <Pressable
+                    onPress={() => setMenuOpen(true)}
+                    style={{ justifyContent: 'center', alignItems: 'center', padding: 10, borderRadius: 10, margin: 5 }}>
                     <Ionicons name='menu' size={25} color={"#fff"} />
                 </Pressable>
             </View>
-            <ScrollView showsVerticalScrollIndicator={true}>
-                <SeparatorComponent title="Favoritos" onDirect='/favoritos'>
-                    <CardFav/>
-                </SeparatorComponent>
-                <SeparatorComponent title="Canais" onDirect='/canais'>
-                    <Card />
-                </SeparatorComponent>
-            </ScrollView>
+            <View>
+
+                <ScrollView showsVerticalScrollIndicator={false}>
+                    <View style={{ gap: 20, padding: 10 }}>
+
+                        <SeparatorComponent title="Favoritos" onDirect='/favoritos'>
+                            <CardFav />
+                        </SeparatorComponent>
+
+                        <SeparatorComponent title="Canais" onDirect='/canais'>
+                            <Card />
+                        </SeparatorComponent>
+
+                    </View>
+                </ScrollView>
+            </View>
             <MenuLateral
-             visible={menuOpen}
-             onClose={() => setMenuOpen(false)}
+                visible={menuOpen}
+                onClose={() => setMenuOpen(false)}
             >
             </MenuLateral>
         </View>
